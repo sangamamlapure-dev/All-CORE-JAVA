41f225b3ef515f9bb0d170be34337630cc7e1808
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-24 14:33:33 +0530

    multi class statics error

---
+++ A_introduction to java/E__multi-class static error/multi.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main (string[] args){
+		system.out.println("hello");
+		codex.fun();
+	}
+}
+
+class method{
+	public void fun (string[] args)
+		system.out.println("world");
+}
