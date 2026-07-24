47b8d47ca67b79e0acff183b6726c508710a11e1
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-24 14:35:26 +0530

    multi class

---
+++ A_introduction to java/F__Multiple class/p1.java
@@ -0,0 +1,13 @@
+//multiple class in same file lihu shakto 
+
+class Demo {
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+}
+class Codex{
+	public static void main(String[] args){
+		System.out.println("world");
+	}
+}
+
---
+++ A_introduction to java/F__Multiple class/p2/two.java
@@ -0,0 +1,17 @@
+class Demo {
+	public static void main(String[] args){
+		codex.fun();
+		System.out.println("in main");
+	}
+}
+class codex{
+	public static void fun(){
+	System.out.println("hello");
+	ics.gun();
+	}
+}
+class ics{
+	public static void gun(){
+	System.out.println("world");
+	}
+}
