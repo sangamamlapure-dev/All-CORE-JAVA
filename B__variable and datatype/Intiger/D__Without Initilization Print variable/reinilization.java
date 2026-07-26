16a1078f0fd6855307602a1755c3ee1424e348da
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-26 20:45:55 +0530

    add without inilization print

---
+++ B__variable and datatype/Intiger/D__Without Initilization Print variable/reinilization.java
@@ -0,0 +1,13 @@
+class Demo{
+	public static void main(String[] args){
+	int a = 20 ;
+	System.out.println(a);
+	Codex.fun();
+	}	
+}
+class Codex{
+	static void fun(){
+	int a = 50;
+	System.out.println(a);
+	}
+}
