7b78e111a154dc34097b7262a17f02638e797f3b
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-26 20:48:19 +0530

    1 class two method

---
+++ B__variable and datatype/Intiger/F__one class two method/p1.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String[] args){
+	 int a = 10;
+	System.out.println(a);
+	}
+	static void fun(){
+	System.out.println(a);
+	}
+}
+
+//print cha area fkta method paryant chalto
