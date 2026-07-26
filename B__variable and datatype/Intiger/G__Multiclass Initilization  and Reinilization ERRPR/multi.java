aca34d3b4b2c5ddedc6ae70920c284e34dad55ad
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-26 20:50:30 +0530

    multi inilizatation and reinilization

---
+++ B__variable and datatype/Intiger/G__Multiclass Initilization  and Reinilization ERRPR/multi.java
@@ -0,0 +1,13 @@
+class Demo{
+	public static void main(String [] args){
+	 int a = 10;
+	 System.out.println(a);
+	 Codex.fun() ;
+	}
+}
+class Codex{
+	static void fun(){
+	 a = 20;
+	 System.out.println(a);
+	}
+}
