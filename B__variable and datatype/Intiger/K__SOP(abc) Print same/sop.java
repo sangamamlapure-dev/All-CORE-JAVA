cc8ec505df723ddfb494b24569b0c1a3214ff818
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-27 23:01:03 +0530

    only prin string and intiger in multiple way

---
+++ B__variable and datatype/Intiger/K__SOP(abc) Print same/sop.java
@@ -0,0 +1,11 @@
+class Demo {
+	public static void main(String[] args){
+	 int a=10 ;
+	 System.out.println(a);
+	 System.out.println(10);
+	 String s = ("abc");
+	 System.out.println(s);
+	 System.out.println("abc");
+	}
+
+}
