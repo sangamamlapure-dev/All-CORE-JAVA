bd2f09a160256819483f0bf7ef273fb9c19705de
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-27 22:39:32 +0530

    same variable print two ways

---
+++ B__variable and datatype/Intiger/I__Same varible print two type/p1.java
@@ -0,0 +1,14 @@
+class Demo{
+	public static void main(String[] args){
+	int a = 10, b = 20 ;
+	System.out.println(a);
+	System.out.println(b);
+	Codex.fun();
+	}
+}
+class Codex{
+	static void fun(){
+	int a = 30;
+	System.out.println(a);
+	}
+}
