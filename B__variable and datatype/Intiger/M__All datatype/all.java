8d8c5c63a79d66b3490fbe48b2da2c8af6b455ef
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-28 22:51:49 +0530

    add all datatype

---
+++ B__variable and datatype/Intiger/M__All datatype/all.java
@@ -0,0 +1,11 @@
+class CodeGurucool{
+	public static void main(String[] args){
+		String s ="shubham";
+		char c = 'A';
+		boolean b = true ;
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(b);	
+	}
+	
+}
