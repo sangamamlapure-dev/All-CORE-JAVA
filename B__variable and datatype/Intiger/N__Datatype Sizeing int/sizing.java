d66b631e3dbb63b68dc31e8bcc4313d2bc57ca98
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-28 22:53:01 +0530

    datatype size

---
+++ B__variable and datatype/Intiger/N__Datatype Sizeing int/sizing.java
@@ -0,0 +1,19 @@
+class Demo{
+	public static void main(String[] args){
+	int a = 'A';
+	System.out.println(a);
+	Demo.fun() ;
+	}
+
+	static void fun(){
+	int a ='a';
+	System.out.println(a);
+	}	
+}
+
+
+
+//  A = intiger vaiue is 65
+//  a = intiger value id 97
+//  b = intiger value is 98 it means 1 ne vadat janar
+// 1byte= 8 bit
