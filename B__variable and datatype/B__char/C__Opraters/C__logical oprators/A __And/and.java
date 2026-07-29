d28289f5009c5f1c1c974716489c26c52fb70cad
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-29 23:16:01 +0530

    add and

---
+++ B__variable and datatype/B__char/C__Opraters/C__logical oprators/A __And/and.java
@@ -0,0 +1,11 @@
+class Demo {
+	public static void main(String[] args){
+		boolean a = true && false ;
+		System.out.println(a);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(true && true);
+		System.out.println(false && false);
+			
+	}
+}
