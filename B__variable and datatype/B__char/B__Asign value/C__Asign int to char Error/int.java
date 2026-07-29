bf0e0695cfc1e2d615cb7dd018af9048ba08c074
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-29 23:02:58 +0530

    add char sizing

---
+++ B__variable and datatype/B__char/B__Asign value/C__Asign int to char Error/int.java
@@ -0,0 +1,10 @@
+class Demo{
+	public static void main(String[] args){
+		int a = 65;
+		char ch = a;
+		System.out.println(a);
+		System.out.println(ch);
+	}
+}
+
+//because of char size is 2 and int is 4 
---
+++ B__variable and datatype/B__char/B__Asign value/D__Swaping/swap.java
@@ -0,0 +1,16 @@
+class CodeGurucool{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 20;
+		int c = b;
+		System.out.println(a);
+		System.out.println(b);
+		
+			System.out.println("After Swap");
+		b= a ;
+		a = c;
+		System.out.println(a);
+		System.out.println(b);
+	
+	}
+}
