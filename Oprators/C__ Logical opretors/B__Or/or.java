1d8e31c6b07f7049521060a74d91e3f6985be1fe
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-30 23:55:05 +0530

    logical oprators

---
+++ Oprators/C__ Logical opretors/B__Or/or.java
@@ -0,0 +1,9 @@
+class Demo {
+	public static void main(String[] args){
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(true || true);
+		System.out.println(false || false);
+
+	}
+}
---
+++ Oprators/C__ Logical opretors/C__ Not/not.java
@@ -0,0 +1,10 @@
+class Demo{
+	public static void main(String[] args){
+		boolean a = ! false ;
+		 boolean b = ! true ;
+		System.out.println(a);
+		System.out.println(b);
+
+
+	}
+}
