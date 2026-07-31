ca2c927c20921d789865eab190f3872d75d43a4a
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-31 22:04:04 +0530

    Move operator examples to C__Oprators

--- C__Oprators/C__ Logical opretors/B__Or/or.java
+++
@@ -1,9 +0,0 @@
-class Demo {
-	public static void main(String[] args){
-		System.out.println(true || false);
-		System.out.println(false || true);
-		System.out.println(true || true);
-		System.out.println(false || false);
-
-	}
-}
