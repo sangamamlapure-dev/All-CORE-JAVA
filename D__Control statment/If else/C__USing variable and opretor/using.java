117ad72e2623df92feab42b1988e9693cfecd8ff
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-31 21:37:18 +0530

    using variable and oprator

---
+++ D__Control statment/If else/C__USing variable and opretor/using.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[] args){
+		int age = 5 ;
+		if(age>= 1){
+			System.out.println("valid");
+		}else{
+			System.out.println("invalid");
+
+		}
+	}
+	
+}
