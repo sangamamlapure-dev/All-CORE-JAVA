abd60420221d91d5763eed70c716e9b4e812bcf7
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-31 21:35:03 +0530

    add if else

---
+++ D__Control statment/If else/A__if true/p1.java
@@ -0,0 +1,14 @@
+class Demo {
+	public static void main(String[] args){
+		
+		if(true){
+			
+			System.out.println("hello") ;
+		}
+		else{
+
+			System.out.println("world") ;
+		
+		}
+	}
+}
---
+++ D__Control statment/If else/B__if false/p2.java
@@ -0,0 +1,13 @@
+class Codegurucool{
+	public static void main(String[] args){
+		
+		if(false){
+		
+				System.out.println("hello");
+			}else{
+				
+				System.out.println("world") ;
+				}
+		
+		}
+}
