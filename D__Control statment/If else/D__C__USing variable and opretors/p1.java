e457c66e99360a0c4c921ecaddc37f3f45504f59
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-07-31 21:44:17 +0530

    using variable and oprator

---
+++ D__Control statment/If else/D__C__USing variable and opretors/p1.java
@@ -0,0 +1,14 @@
+
+class Demo{
+	public static void main(String[] args){
+  		int codes = 2;
+		boolean apti = true;
+			if(codes==1 && apti){
+				System.out.println("Prime package");
+			}else{
+				System.out.println("ninja");			
+			}
+
+		
+	}
+}
