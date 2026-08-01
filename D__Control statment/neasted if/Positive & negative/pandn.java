3a55b9da65d0b4bbf170afe2f86cfac22ae79667
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-08-01 22:30:12 +0530

    positive negative

---
+++ D__Control statment/neasted if/Positive & negative/pandn.java
@@ -0,0 +1,11 @@
+class Codex{
+	public static void main(String[] args){
+		int num = 0 ;
+			if(num >= 0){
+				System.out.println("positive");
+			}else{
+				System.out.println("negative");
+			}
+			
+	}
+}
