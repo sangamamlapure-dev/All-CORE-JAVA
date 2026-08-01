daa19ddf1f585416563f812921509d9533ab146e
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-08-01 22:13:50 +0530

    grade system

---
+++ D__Control statment/if else lader/p1/p1.java
@@ -0,0 +1,12 @@
+class Codex{
+	public static void main(String[] args){
+		int num = 10 ;
+		if(num > 0){
+			System.out.println("positive");
+		}else if(num == 0){
+			System.out.println("zero");
+		}else{
+			System.out.println("negative");
+		}
+	}
+}
