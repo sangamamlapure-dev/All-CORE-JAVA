d9039b7e148c884e33ca4b46886902e6353bc31f
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-08-02 22:46:19 +0530

    username password

---
+++ D__Control statment/neasted if/username password/username.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main(String[] args){
+		String username = "sanga";
+		int password = 123 ;
+			if(username == "sangam" ){
+				if(password == 123){
+					System.out.println("login Successfully");
+				}else{
+					System.out.println("incorrect password");
+				}
+			}else{
+				System.out.println("incorrect username");	
+			}
+	}
+}
