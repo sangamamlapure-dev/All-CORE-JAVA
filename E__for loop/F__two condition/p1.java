68ec99188f148cb2c7e692c2a2c1d836ffd3248f
sangam amlapure <sangam.s.amlapure@gmail.com>
2026-08-03 22:03:34 +0530

    two condition

---
+++ E__for loop/F__two condition/p1.java
@@ -0,0 +1,7 @@
+class codex{
+	public static void main(String[] args){
+			for(int a = 1 ; a<= 8; a++,a=4){
+				System.out.println(a);
+			}
+	}
+}
---
+++ E__for loop/F__two condition/p2.java
@@ -0,0 +1,7 @@
+class codex{
+	public static void main(String[] args){
+			for(int a = 1 ; a<= 8; a=4 , a++){
+				System.out.println(a);
+			}
+	}
+}
