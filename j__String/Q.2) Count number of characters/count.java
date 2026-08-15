88b911e96eb1f2e00203538a2eefabca6ad87370
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-15 23:03:22 +0530

    add number of char

---
+++ j__String/Q.2) Count number of characters/count.java
@@ -0,0 +1,10 @@
+class Codex{
+	public static void main(String[] args){
+		String s="java";
+		int count=0;
+			for(int i=0;i<=s.length()-1;i++){
+				count++;
+			}
+		System.out.println(count);
+	}
+}
