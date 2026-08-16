bc35958ae6835569d0e4a1dd4b9a7254508341ee
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-16 23:03:16 +0530

    count world in string

---
+++ j__String/Q.10) Count words in a string/word.java
@@ -0,0 +1,10 @@
+class Codex{
+	public static void main(String[] args){
+		String[] s={"Java", "DSA", "Spring", "Boot"};
+		int count=0;
+			for(int i=0;i<=s.length-1;i++){
+				count++;
+			}
+		System.out.println(count);
+	}
+}
