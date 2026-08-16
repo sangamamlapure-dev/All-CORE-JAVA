1ccc83b26616672173618503ab7274c4f88bfed7
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-16 23:05:20 +0530

    add frequency of each

---
+++ j__String/Q.11) Find frequency of each character/frequency.java
@@ -0,0 +1,15 @@
+class Codex{
+	public static void main(String[] args){
+		String s="apple";
+		int freq[]=new int[122];
+			for(i=0;i<=s.length()-1;i++){
+				freq[s.charAt(i)]++;	
+			}
+			for(int i=0;i<=freq.length-1;i++){
+				if(freq[i]>0){
+					System.out.println((char)i +"="+ freq[i]);
+				}
+			}
+		System.out.println();
+	}
+}
