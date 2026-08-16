0cf87f32a859755979980086e858bf74cb1a82d9
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-16 22:50:02 +0530

    palindrome

---
+++ j__String/Q.6) Check palindrome string/palinfrome.java
@@ -0,0 +1,16 @@
+class Codex{
+	public static void main(String[] args){
+		String s="madam";
+		String s1="";
+			for(int i=s.length()-1;i>=0;i--){
+				s1=s1+s.charAt(i);		
+			}
+				System.out.println(s1);
+
+		if(s.equals(s1)){
+			System.out.println("palindrome");
+		}else{
+			System.out.println(" not palindrome");
+		}
+	}
+}
