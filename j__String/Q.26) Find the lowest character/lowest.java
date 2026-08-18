09986c46021fa0eb87c87096291fc190fa55a0bc
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-18 22:37:50 +0530

    lowest charecter

---
+++ j__String/Q.26) Find the lowest character/lowest.java
@@ -0,0 +1,14 @@
+class Codex{
+	public static void main(String[] args){
+		String s="bdte";
+			char ch = s.charAt(0);
+
+			for(int i=0;i<=s.length()-1;i++){
+				char c = s.charAt(i);
+				if(ch>c){
+					ch=c;
+				}
+			}
+		System.out.println(ch);
+	}
+}
