bb110862521c13aefb6abd975f0315c114afb498
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-19 23:17:46 +0530

    string compression

---
+++ j__String/Q.29) String compression/compression.java
@@ -0,0 +1,15 @@
+class Codex{
+	public static void main(String[] args){
+		String s="aaabbcccc";
+		int  freq[]= new int[122];
+			for(int i=0;i<=s.length()-1;i++){
+				char ch =s.charAt(i);
+				freq[ch]++;
+			}
+			for(int i=0;i<=freq.length-1;i++){
+				if(freq[i]>0){
+					System.out.println((char)i +"="+ freq[i]);
+				}
+			}
+	}
+}
