ffee4c09481f11a5db89b41953adc9cf59b5f394
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-20 22:28:52 +0530

    alphabate

---
+++ j__String/Q.37) Check if string contains only alphabets/alphabate.java
@@ -0,0 +1,17 @@
+class Codex{
+	public static void main(String[] args){
+		String s="JavaDSA";
+		int count=0;
+			for(int i=0;i<=s.length()-1;i++){
+			  char ch =s.charAt(i);
+				if(ch>='A' && ch<='z'){
+					count++;
+				}
+			}
+		if(count==s.length()){
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}
+	}
+}
