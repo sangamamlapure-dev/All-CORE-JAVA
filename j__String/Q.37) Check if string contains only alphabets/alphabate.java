f0f7267a3947eea7acf82a70b2b4faffcfa2858d
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-21 22:55:59 +0530

    change modified

--- j__String/Q.37) Check if string contains only alphabets/alphabate.java
+++ j__String/Q.37) Check if string contains only alphabets/alphabate.java
@@ -1,8 +1,8 @@
 class Codex{
 	public static void main(String[] args){
 		String s="JavaDSA";
-		int count=0;
-			for(int i=0;i<=s.length()-1;i++){
+		int count =0;
+			for(int i=0;i<=s.length()-1;i++){		
 			  char ch =s.charAt(i);
 				if(ch>='A' && ch<='z'){
 					count++;
