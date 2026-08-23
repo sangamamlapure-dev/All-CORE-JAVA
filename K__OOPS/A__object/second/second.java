0500da774269cbf45e68a0bde45621973b77f543
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-23 22:54:24 +0530

    when you not inilize value give bydefault value also

---
+++ K__OOPS/A__object/second/second.java
@@ -0,0 +1,16 @@
+class Codex{
+	public static void main(String[] args){
+		codeGurucool cg=new codeGurucool();
+			cg.fun();		
+	}
+}
+class codeGurucool{
+	int a;
+	String b;
+	boolean c;
+		void fun(){
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+		}
+}
