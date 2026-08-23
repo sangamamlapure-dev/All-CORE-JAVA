3c7be507ad573e08f0a023edd0d555ff64e1b848
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-23 22:44:31 +0530

    add frist code of object

---
+++ K__OOPS/A__object/frist code/frist.java
@@ -0,0 +1,13 @@
+class Codex{
+	public static void main(String[] args){
+		System.out.println("hello world ");
+		codex cg=new codex();
+			cg.fun();		
+	}
+}
+class codex{
+	int a=10;
+		void fun(){
+			System.out.println("in fun");
+		}
+}
