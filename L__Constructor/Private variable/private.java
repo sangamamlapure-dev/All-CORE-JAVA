9d3af2b0d3c6d9913d4d312722980fac844e0bc6
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-30 23:51:47 +0530

    private varible

---
+++ L__Constructor/Private variable/private.java
@@ -0,0 +1,11 @@
+class Student {
+    private int id;
+}
+
+class Codex {
+    public static void main(String args[]) {
+        Student s = new Student();
+        s.id = 10;   
+        System.out.println(s.id);
+    }
+}
