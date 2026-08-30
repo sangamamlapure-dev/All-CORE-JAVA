6255b4c9caa19a800da7cb74308fa829ca465f46
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-30 23:54:47 +0530

    add accsss from another class

---
+++ L__Constructor/Private variable/accss.java
@@ -0,0 +1,10 @@
+class Employee {
+    private String name = "Sangam";
+}
+
+class Codex {
+    public static void main(String args[]) {
+        Employee e = new Employee();
+        System.out.println(e.name); 
+    }
+}
