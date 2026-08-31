3f791c7e8aa811eabec74bc59b6d89375a217a6d
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-31 23:26:21 +0530

    add change using method

---
+++ L__Constructor/chang using method/change.java
@@ -0,0 +1,22 @@
+class Student{
+	private int age;
+	private String name;
+		Student(int age, String name){
+			this.name=name;
+			this.age=age;
+		}
+		void setage(int setage){
+			 age=setage;
+		}
+		void display(){
+			System.out.println(name);
+			System.out.println(age);
+		}
+}
+class Codex{
+	public static void main(String args[]){
+		Student s1 = new Student(10,"sangam");
+		s1.setage(20);
+		s1.display();
+	}
+}
