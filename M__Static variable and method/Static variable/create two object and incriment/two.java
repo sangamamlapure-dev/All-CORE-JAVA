c20a09733e13ddd4bab28836e36412134902b021
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-07 23:31:49 +0530

    two object and incriment

---
+++ M__Static variable and method/Static variable/create two object and incriment/two.java
@@ -0,0 +1,16 @@
+class Codex{
+	static int age=20;
+}
+class main{
+public static void main(String args[]){
+	Codex c1 = new Codex();
+	System.out.println(c1.age);
+	c1.age++;
+	System.out.println(c1.age);
+
+	Codex c2 = new Codex();
+	System.out.println(c2.age);
+
+		
+	}
+}
