35dafeaefd679a93a857fc5b148645e4c8d1033c
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-11 23:59:02 +0530

    add frist non repeting element

---
+++ I__Introduction to arry/Frequence arry/nonrep.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 1, 2, 1, 4, 5};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(arr[i]);
+                return;
+            }
+        }
+    }
+}
