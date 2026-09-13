554e38c478e0306bd65d5599a5efef135ad4eecc
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-13 23:30:27 +0530

    frist code

---
+++ K__Relasion between two Objects/frist code/frist.java
@@ -0,0 +1,18 @@
+class Student{
+	String name="sangam";
+	void Display(){
+			System.out.println(name);
+		Collage c1= new Collage();
+			System.out.println(c1.cname);
+	}
+	
+}
+class Collage{
+	String cname="kbp";
+}
+class Main{
+	public static void main(String args[]){
+		Student s1=new Student();
+		s1.Display();
+	}
+}
