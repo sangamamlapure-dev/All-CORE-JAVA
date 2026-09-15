6141f7e5be6d19b601f2348c4c7b79c056876d61
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-15 23:03:56 +0530

    paramitri

---
+++ L__Constructor/paramit.java
@@ -0,0 +1,20 @@
+class Student {
+
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student("Sangam", 21);
+        s.display();
+    }
+}
