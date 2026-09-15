0d658b64aab70cd83683c101d9afbb2e79e5c331
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-15 23:00:39 +0530

    default

--- L__Constructor/Private variable/second.java
+++ L__Constructor/Private variable/second.java
@@ -1,14 +1,13 @@
 class Student{
-	private int age=21;;
-	private String name="sangam";
-		Student(int age, String name){
-			this.name=name;
+	private int age;
+	private String name;
+		Student(int age,String name){
 			this.age=age;
+			this.name=name;	
 		}
 }
 class Codex{
 	public static void main(String args[]){
 		Student s1 = new Student(10,"sangam");
-		s1.age=20;
 	}
 }
---
+++ L__Constructor/defult.java
@@ -0,0 +1,10 @@
+class Student {
+
+    Student() {
+        System.out.println("Student Constructor");
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
