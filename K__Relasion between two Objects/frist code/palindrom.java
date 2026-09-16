80575d32b30960bbbc1e3606acdea7748f03c077
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-16 23:40:43 +0530

    add palindrome

---
+++ K__Relasion between two Objects/frist code/palindrom.java
@@ -0,0 +1,18 @@
+class Palindrome {
+    public static void main(String[] args) {
+        int n = 121;
+        int temp = n;
+        int rev = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        if (temp == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
